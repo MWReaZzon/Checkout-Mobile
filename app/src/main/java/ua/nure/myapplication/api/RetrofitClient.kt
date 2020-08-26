d8e184d60b7f0923ba04880ee62466801595392b
Oleksandr Kyivskyi <kievskiyalexandr@gmail.com>
2020-08-26 21:16:57 +0300

    Request header changed

--- app/src/main/java/ua/nure/myapplication/api/RetrofitClient.kt
+++ app/src/main/java/ua/nure/myapplication/api/RetrofitClient.kt
@@ -23,6 +23,7 @@ class RetrofitClient(val lang: String, val auth: String?) {
                 val request = requestBuilder
                     .addHeader("Content-Type", "application/json")
                     .addHeader("Accept-Language", lang)
+                    .addHeader("Connection", "close")
 
                 if (auth != null) {
                     request.addHeader("Authorization", auth)
